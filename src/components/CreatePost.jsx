deef02b4a68b85732b7d02d5dee2eec262199655
Saim Khan <saimkhan065@gmail.com>
2023-04-13 02:11:32 -0400

    added submitHandler on form text to test state change

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,12 +1,19 @@
+import {useState} from 'react';
 import classes from './CreatePost.module.css'
 
 function CreatePost(){
+    const [current_state, set_current_state] = useState('');
+    function submitHandler(event){
+    set_current_state(event.target.value);
+    }
+
     return(
       <form className={classes.form}>
           <p>
               <label htmlFor="body">Description</label>
-              <textarea name="" id="body" cols="30" rows="10" required></textarea>
+              <textarea name="" id="body" cols="30" rows="10" required onChange={submitHandler}></textarea>
           </p>
+          <p>{current_state}</p>
           <p>
               <label htmlFor="name">Task Owner</label>
               <input type="text" id="name" required/>
